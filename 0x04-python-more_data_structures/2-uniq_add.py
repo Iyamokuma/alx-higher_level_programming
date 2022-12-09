7e8389b3c5509bef067b273d0861afb4afb3c2a5
Iyamokuma <virtuehorsefall@gmail.com>
2022-12-09 06:07:48 -0800

    my gut

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    result = 0
+    for x in set(my_set):
+        result += x
+    return result
+
+
+'''
+def uniq_add(my_list=[]):
+    return sum(set(my_list)):
+    return sum(set(my_list))
+'''
