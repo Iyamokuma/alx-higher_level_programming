12b1b8f265415e1abf9882dabf3f995794e2ddea
Iyamokuma <virtuehorsefall@gmail.com>
2023-03-16 09:47:25 -0700

    my guy

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const x = process.argv[2];
 const change = parseInt(x);
 if (isNaN(change)) {
-	 console.log('Missing number of occurrences');
-	} else {
-	   for (let i = 0; i < x; i++) { console.log('C is fun'); }
-	}
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) { console.log('C is fun'); }
+}
