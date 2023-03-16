fa77c50d35ed9548db8821ffc97d854bf8fd7226
Iyamokuma <virtuehorsefall@gmail.com>
2023-03-16 09:25:24 -0700

    my guy

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const myVar = process.argv[2];
 const change = parseInt(myVar);
 if (isNaN(change)) {
-  console.log('Not a number');
+   console.log('Not a number');
 } else {
  console.log(`My number: ${change}`);
 }
