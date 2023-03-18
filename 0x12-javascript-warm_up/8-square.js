1b9c2195121b165caa525c05f13c80005341c24b
Iyamokuma <virtuehorsefall@gmail.com>
2023-03-18 02:37:35 -0700

    my guy

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 const x = process.argv[2];
 const change = parseInt(x);
 if (isNaN(change)) {
-	  console.log('Missing size');
-	} else {
-		for (let i = 0; i < x; i++) {
-			 console.log('X'.repeat(x));
-		}
-	}
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+}
